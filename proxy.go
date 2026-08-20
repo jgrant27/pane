6e4e556673a9f26953da8f4482b7dd34012fda6f
Justin Grant <jgrant27@gmail.com>
2026-08-20 06:56:37 -0700

    Bump version to v0.2.4

--- proxy.go
+++ proxy.go
@@ -129,7 +129,7 @@ func (p *proxy) ensureHub() (*agentHub, error) {
 		"clientInfo": map[string]string{
 			"name":    "grok-pane",
 			"title":   "Grok Pane",
-			"version": "0.2.3",
+			"version": "0.2.4",
 		},
 		"clientCapabilities": map[string]any{
 			"fs":       map[string]bool{"readTextFile": false, "writeTextFile": false},
