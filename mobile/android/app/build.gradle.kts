7d9f5db9e82a7a4f7407ac33d8bdb3749808fbb1
Justin Grant <jgrant27@gmail.com>
2026-09-10 07:30:56 -0700

    Bump version to v0.2.16

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         targetSdk = 35
         // both are stamped by cmd/bump: versionName from VERSION, versionCode from
         // it as major*1000000 + minor*1000 + patch so Play always sees it climb.
-        versionCode = 2015
-        versionName = "0.2.15"
+        versionCode = 2016
+        versionName = "0.2.16"
     }
     buildTypes {
         release {
